9252ceed829f78b29f9a2a5a1452635fd0771183
Bogdan Voicu <bogdancristivoicu@gmail.com>
2020-05-03 12:16:31 +0300

    Added answer correctness check and using setState

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 import QuizQuestionButton from "./QuizQuestionButton.js";
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      incorrectAnswer: false
+    };
+  }
   render() {
     return (
       <main>
@@ -21,6 +27,9 @@ class QuizQuestion extends Component {
             })}
           </ul>
         </section>
+        {this.state.incorrectAnswer ? (
+          <p className="error">Sorry, that's not right</p>
+        ) : null}
       </main>
     );
   }
@@ -28,6 +37,13 @@ class QuizQuestion extends Component {
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
       this.props.showNextQuestionHandler();
+      this.setState({
+        incorrectAnswer: false
+      });
+    } else {
+      this.setState({
+        incorrectAnswer: true
+      });
     }
   }
 }
