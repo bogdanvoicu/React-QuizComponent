f2e39207eb87ecdfeb1e355eb26042f5fb6ee7f4
Bogdan Voicu <bogdancristivoicu@gmail.com>
2020-05-03 11:47:24 +0300

    Add onclick events and handlers

--- src/Quiz.js
+++ src/Quiz.js
@@ -21,10 +21,15 @@ class Quiz extends Component {
             quiz_question={
               quizData.quiz_questions[this.state.quiz_position - 1]
             }
+            showNextQuestionHandler={this.showNextQuestion.bind(this)}
           />
         )}
       </div>
     );
+  }
+
+  showNextQuestion() {
+    this.state.quiz_position = this.state.quiz_position + 1;
   }
 }
 
